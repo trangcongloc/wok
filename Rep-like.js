13629dc1cefbb2ac93894b56ce8422a191a3ebe3
trangcongloc <50675760+trangcongloc@users.noreply.github.com>
2023-11-01 17:44:33 +0700

    Create Rep-like.js

---
+++ Rep-like.js
@@ -0,0 +1,26 @@
+// Tool auto like + reply
+document.querySelectorAll('[aria-label="Heart"]').forEach(heart=>heart.click())
+console.log("Liked")
+
+setTimeout(() => {
+    console.log("Choosing reply...")
+    let secondSg = document.querySelectorAll("#suggestion-1");
+    secondSg.forEach(sgt => {
+        sgt.addEventListener('click', (event) => {
+            event.stopPropagation(); // Stop event propagation
+        });
+    sgt.click(); 
+    });
+
+    setTimeout(() => {
+        console.log("Reply added...")
+        let btnElement = document.querySelectorAll(".style-scope.ytcp-comment-button.style-blue-text.size-default");
+        btnElement.forEach(btn => {
+        btn.addEventListener('click', (event) => {
+            event.stopPropagation(); // Stop event propagation
+        });
+        btn.click();
+        });
+    }, 3000); 
+}, 1000)
+
